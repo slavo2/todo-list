b8318f4de7b207e2169c93d757c9c2255fd9b6b7
Slavo <slavo@localhost>
2023-09-21 18:02:48 +0200

    Add methods for adding and checking user
    into users.service
    
    - remove methods findAll, findOneByUsername, findOneById
    - add methods addUser, isUsernameUsed, findAndCheckUser
    - add private method findOneByUsernameKeepPasswordHash

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,23 +2,63 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
+    saltOrRounds = 10;
+
     constructor(
         @InjectRepository(User)
         private usersRepository: Repository<User>,
     ) { }
-    
-    findAll(): Promise<User[]> {
-        return this.usersRepository.find();
+
+    async addUser(username: string, password: string): Promise<User | false | null> {
+        if (!username || !password) {
+
+            throw new Error('username and password are required');
+        }
+        if (await this.isUsernameUsed(username)) {
+
+            return false;
+        }
+        const hash = await bcrypt.hash(password, this.saltOrRounds);
+        const user = new User();
+        user.username = username;
+        user.passwordHash = hash;
+        this.usersRepository.save(user);
+        const { passwordHash, ...result } = user;
+
+        return result;
     }
 
-    findOneByUsername(username: string): Promise<User | null> {
+    private findOneByUsernameKeepPasswordHash(username: string): Promise<User | null> {
         return this.usersRepository.findOneBy({ username });
     }
 
-    findOneById(id: string): Promise<User | null> {
-        return this.usersRepository.findOneBy({ id });
+    async isUsernameUsed(username: string): Promise<boolean> {
+        const user = await this.findOneByUsernameKeepPasswordHash(username);
+
+        return user ? true : false;
+    }
+   
+    async findAndCheckUser(username: string, password: string): Promise<User | null > {
+        if (!username || !password) {
+
+            throw new Error('username and password are required');
+        }
+        const user = await this.findOneByUsernameKeepPasswordHash(username);
+        if (!user) {
+
+            return null;
+        }
+        const match = await bcrypt.compare(password, user.passwordHash);
+        if (!match) {
+
+            return null;
+        }
+        const { passwordHash, ...userWithoutPasswordHash } = user;
+    
+        return userWithoutPasswordHash;
     }
 }
