65bc4baa22fcd917e0651b9f320269393d0912a4
Slavo <slavo@localhost>
2023-09-21 15:09:04 +0200

    Add entity User and related service methods
    - update users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./user.entity";
+import { UsersService } from "./users.service";
+
 
 @Module({
-  providers: [UsersService]
+    imports: [TypeOrmModule.forFeature([User])],
+    providers: [UsersService],
+    controllers: [],
+    exports: [TypeOrmModule, UsersService]
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) { }
+    
+    findAll(): Promise<User[]> {
+        return this.usersRepository.find();
+    }
+
+    findOneByUsername(username: string): Promise<User | null> {
+        return this.usersRepository.findOneBy({ username });
+    }
+
+    findOneById(id: string): Promise<User | null> {
+        return this.usersRepository.findOneBy({ id });
+    }
+}
