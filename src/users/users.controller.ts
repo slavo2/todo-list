a34f97a1eb338b37540f801d8204b57fcfa9b148
Slavo <slavo@localhost>
2023-11-23 18:17:45 +0100

    Make user registration available without authentication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
 import { BadRequestException, Body, Controller, Post } from "@nestjs/common";
 import { RegisterUserDto } from "./register-user.dto";
 import { UsersService } from "./users.service";
+import { Public } from "src/auth/public.decorator";
 
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) { }
     
+    @Public()
     @Post()
     async register(@Body() registerUserDto: RegisterUserDto) {
         const result = await this.usersService.addUser(registerUserDto.username, registerUserDto.password);
