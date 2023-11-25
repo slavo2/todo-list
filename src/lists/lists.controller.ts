8c3e7eddfc5725b5c864467684b5aaa0a33b3a59
Slavo <slavo@localhost>
2023-11-25 17:57:25 +0100

    Mark non GET lists endpoints with Bearer authentication

--- src/lists/lists.controller.ts
+++ src/lists/lists.controller.ts
@@ -3,11 +3,13 @@ import { ListsService } from './lists.service';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
 import { Public } from 'src/auth/public.decorator';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('lists')
 export class ListsController {
   constructor(private readonly listsService: ListsService) {}
 
+  @ApiBearerAuth()
   @Post()
   create(@Body() createListDto: CreateListDto) {
     return this.listsService.create(createListDto);
@@ -25,11 +27,13 @@ export class ListsController {
     return this.listsService.findOne(+id);
   }
 
+  @ApiBearerAuth()
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateListDto: UpdateListDto) {
     return this.listsService.update(+id, updateListDto);
   }
 
+  @ApiBearerAuth()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.listsService.remove(+id);
