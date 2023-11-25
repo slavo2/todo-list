edfda94fefa7e2f0fdf5e85c5eff61e49e0c3b08
Slavo <slavo@localhost>
2023-11-25 17:24:27 +0100

    Make root path / redirecting to /api docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, HttpCode, HttpStatus, Redirect } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
+import { Public } from './auth/public.decorator';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @HttpCode(HttpStatus.SEE_OTHER)
+  @ApiResponse({ status: HttpStatus.SEE_OTHER, description: 'Redirect to API documentation.' })
+  @Redirect('/api', HttpStatus.SEE_OTHER)
+  redirectRootToApiDocs() { }  
 }
